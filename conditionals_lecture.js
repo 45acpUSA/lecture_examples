5e921db63f587e3c9189068d68aac684da6cb4f2
Sarah Proctor <sproctor950@gmail.com>
2019-04-16 16:48:34 -0700

    added lecture notes repo and conditonals and arrays lecture

---
+++ conditionals_lecture.js
@@ -0,0 +1,17 @@
+//---------Conditionals Lecture 4/16/19
+
+var loggedIn = true
+
+if(loggedIn === true){
+    console.log("You are logged in!")
+} else {
+    console.log("You are not logged in.")
+}
+
+if(loggedIn === false){
+    console.log("Not logged in")
+} else if(loggedIn === true){
+    console.log("Logged in")
+} else {
+    console.log("there was an error")
+}
